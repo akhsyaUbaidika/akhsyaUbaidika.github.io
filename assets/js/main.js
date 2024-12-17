b04c6f2cf64e9173798575c807876370ae8d08f7
akhsyaUbaidika <152408578+akhsyaUbaidika@users.noreply.github.com>
2024-12-17 23:58:07 +0700

    Update portfolio to 2024 version

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+// import Typed from 'typed.js';
+// // Typed.js Initiation
+// if (document.querySelector('.header-text h3')) {
+//     var typed_strings = document.querySelector('.header-text .typed-text').textContent;
+//     new Typed('.header-text h3', {
+//         strings: typed_strings.split(', '), // Pisahkan teks berdasarkan koma
+//         typeSpeed: 100,                     // Kecepatan mengetik (ms)
+//         backSpeed: 50,                      // Kecepatan menghapus (ms)
+//         loop: true                          // Aktifkan pengulangan animasi
+//     });
+// }
